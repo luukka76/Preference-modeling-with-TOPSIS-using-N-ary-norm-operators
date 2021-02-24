ccd5d2620b31eeaac14c287310c8a399dffac54b
Pasi Luukka <54097720+luukka76@users.noreply.github.com>
2021-02-24 15:52:22 +0200

    Update narytopsis.m

--- narytopsis.m
+++ narytopsis.m
@@ -1,20 +1,27 @@
-function [c,Dplus,Dminus]=topsis(data,p,w,w2,crit)
+function [c,Dplus,Dminus]=topsis(data,p,w,w2,crit,we)
 x=data;
 [m,n]=size(x);
+if nargin<6
+we=ones(1,n);
+elseif nargin<5
+crit=ones(1,n);
+end
+
 a=zeros(m,n);
 %Normalization to unit interval:
 a=normalization(x);
-
+%weigted normalized decision matrix:
+r=we.*a;
 %Positive and negative ideal solutions:
 PIS=zeros(1,n);
 NIS=zeros(1,n);
 for j=1:n
      if crit(j)==1   
-        PIS(j)=recursivetconorm(a(:,j)',w);
-        NIS(j)=recursivetnorm(a(:,j),w2);
+        PIS(j)=recursivetconorm(r(:,j)',w);
+        NIS(j)=recursivetnorm(r(:,j),w2);
      else
-        NIS(j)=recursivetconorm(a(:,j)',w);
-        PIS(j)=recursivetnorm(a(:,j),w2);    
+        NIS(j)=recursivetconorm(r(:,j)',w);
+        PIS(j)=recursivetnorm(r(:,j),w2);    
      end
 end
 
@@ -22,8 +29,8 @@ end
  DPISB=zeros(1,m);
  DNISB=zeros(1,m);
 for i=1:m
-   Dplus(i)=distM(PIS,a(i,:),p); 
-   Dminus(i)=distM(NIS,a(i,:),p); 
+   Dplus(i)=distM(PIS,r(i,:),p); 
+   Dminus(i)=distM(NIS,r(i,:),p); 
 end
 %Closeness coefficient: 
  c=zeros(1,m);
